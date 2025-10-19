4fe84ba27c34c6073295488316d76ff0cb087d4e
Max Gontar <max.gontar@gmail.com>
2025-10-19 19:19:50 +0300

    The echo builtin

--- app/main.py
+++ app/main.py
@@ -18,11 +18,21 @@ def main():
             exit_code = int(match.group(1))
             sys.exit(exit_code)
 
-        # Format an output
-        output = f"{user_input}: command not found"
-
-        # Print an output
-        print(output)
+        # Check for exit command
+        pattern = r"^echo\s+(.+)"
+        match = re.search(pattern, user_input)
+        if match:
+            args_string = match.group(1)
+            args_list = args_string.split()
+            args_list_out = [s.strip() for s in args_list]
+            args_string_out = " ".join(args_list_out)
+            output = f"{args_string_out}"
+            print(output)
+        else:
+            # Format an output
+            output = f"{user_input}: command not found"
+            # Print an output
+            print(output)
 
 
 if __name__ == "__main__":
