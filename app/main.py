e667c35faaa16eaaa305fb26d8b3ae2100759d51
Max Gontar <max.gontar@gmail.com>
2025-10-19 20:09:21 +0300

    The type builtin: builtins

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ import sys
 import re
 
 def main():
+    builtin_commands_list = ["exit", "echo", "type"]
 
     # Never ending REPL loop
     while True:
+
+        command_detected = False
+
         # Print a prompt
         sys.stdout.write("$ ")
 
@@ -15,6 +19,7 @@ def main():
         pattern = r"^exit\s+(\d{1})"
         match = re.search(pattern, user_input)
         if match:
+            command_detected = True
             exit_code = int(match.group(1))
             sys.exit(exit_code)
 
@@ -22,13 +27,28 @@ def main():
         pattern = r"^echo\s+(.+)"
         match = re.search(pattern, user_input)
         if match:
+            command_detected = True
             args_string = match.group(1)
             args_list = args_string.split()
             args_list_out = [s.strip() for s in args_list]
             args_string_out = " ".join(args_list_out)
             output = f"{args_string_out}"
             print(output)
-        else:
+
+        # Check for exit command
+        pattern = r"^type\s+(\w+)"
+        match = re.search(pattern, user_input)
+        if match:
+            command_detected = True
+            arg = match.group(1)
+            if arg in builtin_commands_list:
+                output = f"{arg} is a shell builtin"
+                print(output)
+            else:
+                output = f"{arg}: not found"
+                print(output)
+
+        if not command_detected:
             # Format an output
             output = f"{user_input}: command not found"
             # Print an output
