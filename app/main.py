0aacb9beab13fe234fe4094645b36f606a6a416b
Max Gontar <max.gontar@gmail.com>
2025-10-20 10:28:17 +0300

    The type builtin: executable files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import sys
 import re
+import os
+from pathlib import Path
 
 def main():
     builtin_commands_list = ["exit", "echo", "type"]
@@ -40,13 +42,33 @@ def main():
         match = re.search(pattern, user_input)
         if match:
             command_detected = True
+
+            command_handled = False
+
             arg = match.group(1)
-            if arg in builtin_commands_list:
-                output = f"{arg} is a shell builtin"
-                print(output)
-            else:
-                output = f"{arg}: not found"
-                print(output)
+
+            path_variable = os.getenv("PATH", "")
+            path_list = path_variable.split(os.pathsep)
+
+            for path in path_list:
+                if os.path.isdir(path):
+                    files = [f for f in Path(path).iterdir() if f.is_file()]
+                    for file in files:
+                        if os.access(file, os.X_OK) and file.name == arg:
+                            command_handled = True
+                            output = f"{arg} is {file}"
+                            print(output)
+                            break
+                    if command_handled:
+                        break
+
+            if not command_handled:
+                if arg in builtin_commands_list:
+                    output = f"{arg} is a shell builtin"
+                    print(output)
+                else:
+                    output = f"{arg}: not found"
+                    print(output)
 
         if not command_detected:
             # Format an output
