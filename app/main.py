346648e23400830739b561c89ad115d8134a5335
Max Gontar <max.gontar@gmail.com>
2025-10-20 10:51:19 +0300

    Run a program

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
+import subprocess
 import sys
 import re
 import os
 from pathlib import Path
+
+def get_executables(name):
+    executables_list = []
+
+    path_variable = os.getenv("PATH", "")
+    path_list = path_variable.split(os.pathsep)
+
+    for path in path_list:
+        if os.path.isdir(path):
+            files = [f for f in Path(path).iterdir() if f.is_file()]
+            for file in files:
+                if os.access(file, os.X_OK) and file.name == name:
+                    executables_list.append(file)
+    return executables_list
 
 def main():
     builtin_commands_list = ["exit", "echo", "type"]
@@ -53,29 +68,38 @@ def main():
                 print(output)
 
             if not command_handled:
-                path_variable = os.getenv("PATH", "")
-                path_list = path_variable.split(os.pathsep)
-
-                for path in path_list:
-                    if os.path.isdir(path):
-                        files = [f for f in Path(path).iterdir() if f.is_file()]
-                        for file in files:
-                            if os.access(file, os.X_OK) and file.name == arg:
-                                command_handled = True
-                                output = f"{arg} is {file}"
-                                print(output)
-                                break
-                        if command_handled:
-                            break
+                executables_list = get_executables(arg)
+                if len(executables_list) > 0:
+                    command_handled = True
+                    output = f"{arg} is {executables_list[0]}"
+                    print(output)
                 else:
                     output = f"{arg}: not found"
                     print(output)
 
         if not command_detected:
-            # Format an output
-            output = f"{user_input}: command not found"
-            # Print an output
-            print(output)
+            pattern = r"^(\w+)\s*(.*)"
+            match = re.search(pattern, user_input)
+            if match:
+                arg_name = match.group(1)
+                arg_string = match.group(2)
+                arg_list = arg_string.split()
+                arg_list_out = [s.strip() for s in arg_list]
+                executables_list = get_executables(arg_name)
+
+                if len(executables_list) > 0:
+                    arg_list_out.insert(0, executables_list[0].name)
+                    subprocess.run(arg_list_out)
+                else:
+                    # Format an output
+                    output = f"{user_input}: command not found"
+                    # Print an output
+                    print(output)
+            else:
+                # Format an output
+                output = f"{user_input}: command not found"
+                # Print an output
+                print(output)
 
 
 if __name__ == "__main__":
